2e1cb3915b781584af897e4f0947cbfea5c0de53
Jincy Jeevan <2000ashnajeevan@gmail.com>
2022-05-24 12:28:17 +0530

    if else sample

---
+++ ifelse.js
@@ -0,0 +1,102 @@
+// A number is a multiple of 7 or not
+
+// let num=prompt("enter a number:");
+// console.log(num);
+// if(num%7==0){14
+//     console.log("entered number is a multiple of 7");
+
+// }else{
+//     console.log("entered number is not a multiple of 7");
+
+// }
+
+// Number is positive or negative
+
+// let num=prompt("Enter a number");
+// console.log(num);
+// if(num>0){
+//     console.log("The entered number is positive");
+// }
+// else{
+//     console.log("The entered number is negative");
+// }
+
+// Even or odd
+
+// let num=prompt("Enter a number");
+// console.log(num);
+// if(num%2==0){
+//     console.log("The entered numer is even");
+// }else{
+//     console.log("The entered number is odd");
+// }
+
+// leap year or not
+
+// let year=prompt("Enter a year");
+// console.log(year);
+// if(year%4==0){
+//     console.log("The entered year is a leap year");
+// }else{
+//     console.log("The entered year is not a leap year");
+// }
+
+// Largest of two numbers
+
+// let num1=prompt("Enter a number");
+// let num2=prompt("Enter another number");
+// if(num1>num2){
+//     console.log(num1);
+// }else{
+//     console.log(num2 );
+// }
+
+// Divisible by 7 and 5
+// let number=prompt("enter a number");
+// console.log(number);
+// if(number%5==0 && number%7==0){
+//     console.log("Entered number is divisible by 5 and 7");
+// }else{
+//     console.log("Entered number is not divisible by 5 and 7");
+// }
+
+// vowel or consonant
+
+// var alpha=prompt("Enter an alphabet");
+// console.log(alpha);
+// if(alpha=='a'|| alpha=='e'|| alpha=='i'|| alpha=='o'|| alpha=='u' || alpha=='A'|| alpha=='E' || alpha=='I' || alpha=='O' || alpha=='U'){
+//     console.log("Entered alphabet is a vowel");
+// }else{
+//     console.log("entered alphabet is a consonant");
+// }
+
+// character or not
+// var char=prompt("enter a character");
+// console.log(char);
+// if(char<=122 && char>=97 || char<=90 && char>=65){
+//     console("Entered value is a character")
+
+// }else{
+//     console.log("Entered value is not a character");
+// }
+
+// weekday
+let date=prompt("Enter a weekday number");
+console.log(date);
+if(date==1){
+    console.log("Sunday");
+}else if (date==2){
+    console.log("Monday");
+}else if(date==3){
+    console.log("Tuesday");
+}else if(date==4){
+    console.log("Wedensday");
+}else if(date==5){
+    console.log("Thursday");
+}else if(date==6){
+    console.log("friday");
+}else if(date==7){
+    console.log("saturday");
+}else{
+    console.log("Invalid date");
+}
