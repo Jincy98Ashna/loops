bc56fb265eca99f31da3c6875945883bc2bf6f0f
Jincy Jeevan <2000ashnajeevan@gmail.com>
2022-05-24 14:32:20 +0530

    new git

---
+++ forloop.js
@@ -0,0 +1,50 @@
+// display 10 numbers
+// for(i=0;i<=10;i++){
+//     console.log(i);
+// }
+
+// sum of 5 numbers
+// let sum=0;
+// for(i=1;i<=5;i++){
+//     sum=sum+i;
+//     console.log(i);
+    
+// }console.log(sum);
+
+// while loop
+
+// let i=0;
+// while(i<=5){
+    
+//     console.log(i);
+//     i++;
+// }
+
+// first 10 even numbers
+// let i=2;
+// while(i<=15){
+//     if(i%2==0){
+//         console.log(i);
+
+//     }
+    
+    
+//     i++
+// }
+
+// do-while
+// i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<=10)
+
+// odd numbers
+i=1;
+do{
+    console.log(i);
+    i++;
+}while (i<=10);
+if(i%2!=0){
+    
+}
